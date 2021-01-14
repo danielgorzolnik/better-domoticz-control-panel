eba0e7bce1db4e987b4c4b07e3fedacb7a3c926c
Daniel Gorzolnik <danielgorzolnik@gmail.com>
2021-01-14 11:33:15 +0100

    login page

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, send_file, flash
+from flask import Flask, render_template, request, send_file, flash, redirect, url_for
 from flask_socketio import SocketIO, emit
 from flask_fontawesome import FontAwesome
+import flask_login
 import domoticzCommunication, database
 import time, json
 
@@ -8,10 +9,66 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 fa = FontAwesome(app)
 
-domoticz = domoticzCommunication.DomoticzCommuniucation()
+#authorization
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+app.secret_key = 'super secret string'  # Change this!
 
-@app.route('/')
-def home():
+users = {'foo@bar.tld': {'password': 'secretplik111'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def load_user(email):
+  if email not in users:
+    return
+  user = User()
+  user.id = email
+  return user
+
+@login_manager.request_loader
+def requestLoader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+    user = User()
+    user.id = email
+    user.is_authenticated = request.form['password'] == users[email]['password']
+    return user
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+               <form action='/' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+    email = request.form['email']
+    try:
+      if request.form['password'] == users[email]['password']:
+          user = User()
+          user.id = email
+          flask_login.login_user(user)
+          return redirect(url_for('panel'))
+      return login()
+    except Exception as e:
+      print(e)
+      return login()
+
+#end of authorization
+
+@app.route('/data')
+def names():
+    data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
+    return jsonify(data)
+
+@app.route('/panel')
+@flask_login.login_required
+def panel():
   if domoticz.connectStatus:
     return render_template('desktop.html')
   else: 
@@ -52,6 +109,8 @@ def clickBlind(data):
   domoticz.changeCover(int(data['idx']), str(data['state']))
   getStatusOfFavoriteDevicesLight() #send updated statuses
 
+domoticz = domoticzCommunication.DomoticzCommuniucation()
+
 if __name__ == '__main__':
   a = database.RemoteDatabase()
-  socketio.run(app, host='0.0.0.0', port=82)
+  socketio.run(app, host='0.0.0.0', port=81)
